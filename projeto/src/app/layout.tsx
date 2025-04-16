7e9dffef7b1ce1a8d1fb308c5e303c525b924450
alissonhfp2003 <alissonhfp2003@gmail.com>
2025-04-16 03:29:08 -0300

    metadata

--- projeto/src/app/layout.tsx
+++ projeto/src/app/layout.tsx
@@ -1,6 +1,18 @@
 import "./globals.css";
 
 import {Header} from "../components/header"
+import { Metadata
+
+ } from "next";
+export const metadata: Metadata ={
+  title: "Projeto de estudo",
+  description:"Projeto feito para estudar next.js",
+  openGraph: {
+    title: "Projeto de estudo",
+    description: "Projeto feito para estudar next.js",
+  }
+
+}
 
 export default function RootLayout({
   children,
